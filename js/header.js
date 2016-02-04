f0e1529474facd7792ca6a07dfe9193974ddc435
maycheng <maycheng@Mays-MacBook-Pro.local>
2016-02-04 17:52:31 -0600

    initial commit

---
+++ js/header.js
@@ -0,0 +1,12 @@
+
+
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 600) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".header").removeClass("active");
+        }
+    });
+});
