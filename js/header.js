f5e2a184fb797c899281ca07814bd4e0726828d0
maycheng <maycheng@Mays-MacBook-Pro.local>
2016-02-17 15:51:27 -0500

    Updates
    Color, header transparency, minor adjustments

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@
 
 $(function() {
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 600) {
+        if($(window).scrollTop() > 510) {
             $(".header").addClass("active");
         } else {
             //remove the background property so it comes transparent again (defined in your css)
